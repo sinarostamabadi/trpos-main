dc3fb28e979ef22ca3489feb4a5f31aef3c467f8
Farimah Fattahi <farimah.fattahi2000@gmail.com>
2024-05-24 15:49:09 +0330

    Edit drop down sidebar menu feature

--- src/components/menu/components/menu-item.tsx
+++ src/components/menu/components/menu-item.tsx
@@ -13,17 +13,22 @@ export const MenuItem: React.FC<MenuItemProps> = ({
   isToggled,
   children,
 }) => {
-  const [isOpen , setIsOpen]=useState<boolean>(false);
-  const pathName = useLocation().pathname;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { pathname } = useLocation();
+
+  const pathFinder = () => {
+    const result = children?.filter((ch) => ch.href === pathname);
+    return result && result[0]?.href;
+  };
+
   return (
-    
     <>
       <li
-      onClick={() => {
-        if(children) {
-          setIsOpen(!isOpen);
-        }
-      }}
+        onClick={() => {
+          if (children) {
+            setIsOpen(!isOpen);
+          }
+        }}
         className={`flex items-center gap-2 py-2 my-3 cursor-pointer select-none ${
           isActive ? "text-base-content" : "text-base-content-40"
         }`}
@@ -32,47 +37,54 @@ export const MenuItem: React.FC<MenuItemProps> = ({
           <div className="h-9 w-[6px] absolute left-0 rounded-r-3xl bg-base-content"></div>
         )}
 
-        <Link to={href!} className={`flex gap-x-2 ${children && "pointer-events-none"}`}>
+        <Link
+          to={href!}
+          className={`flex gap-x-2 ${children && "pointer-events-none"} ${
+            children && pathFinder() && " text-base-content"
+          }`}
+        >
           {iconActive && isActive ? iconActive : iconDeactive}
           {isToggled && title}
         </Link>
 
         {isToggled && isParent && (
-          isOpen ?
           <IconArrowDown
             width={20}
             height={20}
             viewBox="0 0 20 20"
-            className="ml-auto rotate-180 mb-1"
-          /> :
-          <IconArrowDown
-            width={20}
-            height={20}
-            viewBox="0 0 20 20"
-            className="ml-auto"
+            className={`ml-auto rotate-180 mb-1 duration-200 ${
+              isOpen && "rotate-0 mb-0"
+            } ${isOpen && pathFinder() && "text-base-content"}`}
           />
         )}
       </li>
-      {
-        children && isOpen &&
-        <div className="flex flex-col gap-6 border-b pb-4 pl-1">
-          {
-            children?.map((item) => {
-              return (
-                <Link to={item.href!} className={`${pathName===item.href ? "text-base-content flex gap-4 items-center" : "text-base-content-40 pl-7"}`}>
-                  {
-                    pathName===item.href &&
-                    <div className="w-[14px] h-1 bg-base-content rounded-2.5xl">
-                    
-                    </div>
-                  }
-                  {item.title}
-                </Link>
-              )
-            })
-          }
-        </div>
-      }
+
+      <div
+        className={`flex flex-col gap-6 duration-300 h-0 overflow-hidden ${
+          isOpen ? "border-b pb-4 pl-1 h-36" : ""
+        }`}
+      >
+        {children &&
+          isOpen &&
+          children?.map((item, index) => {
+            return (
+              <Link
+                key={item.title + index}
+                to={item.href!}
+                className={`${
+                  pathname === item.href
+                    ? "text-base-content flex gap-4 items-center"
+                    : "text-base-content-40 pl-7"
+                }`}
+              >
+                {pathname === item.href && (
+                  <div className="w-[14px] h-1 bg-base-content rounded-2.5xl"></div>
+                )}
+                {item.title}
+              </Link>
+            );
+          })}
+      </div>
     </>
   );
 };
